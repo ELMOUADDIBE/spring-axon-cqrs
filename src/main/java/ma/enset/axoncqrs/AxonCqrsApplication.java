fdd9c6d5d3ab5f00008a3e0d66ac53be3278d134
EL MOUADDIBE <elmouaddibezaid@gmail.com>
2024-12-29 15:13:46 +0100

    Initial commit

---
+++ src/main/java/ma/enset/axoncqrs/AxonCqrsApplication.java
@@ -0,0 +1,13 @@
+package ma.enset.axoncqrs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AxonCqrsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AxonCqrsApplication.class, args);
+    }
+
+}
---
+++ src/main/java/ma/enset/axoncqrs/commonApi/commands/commands.kt
@@ -0,0 +1,27 @@
+package ma.enset.axoncqrs.commonApi.commands
+
+import org.axonframework.modelling.command.TargetAggregateIdentifier
+
+
+abstract class BaseCommand<T>(
+    @TargetAggregateIdentifier
+    open val id: T
+)
+
+data class CreateAccountCommand(
+    override val id: String,
+    val balance: Double,
+    val currency: String
+) : BaseCommand<String>(id)
+
+data class UpdateAccountCommand(
+    override val id: String,
+    val balance: Double
+) : BaseCommand<String>(id)
+
+data class CreditAccountCommand(
+    override val id: String,
+    val amount: Double,
+    val currency: String
+) : BaseCommand<String>(id)
+
